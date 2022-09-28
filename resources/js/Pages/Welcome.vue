35a7b55aaa928c6ee4f18af697e74bf2ff7d1fab
Ndirangu Waweru <4353067+nwaweru@users.noreply.github.com>
2022-09-28 16:55:39 +0300

    fixed overflow in welcome page

--- resources/js/Pages/Welcome.vue
+++ resources/js/Pages/Welcome.vue
@@ -1,7 +1,7 @@
 <template>
     <Head title="Welcome" />
 
-    <div class="controller">
+    <div class="container">
         <div class="row vh-100 align-items-center">
             <div class="col-12">
                 <h1 class="text-center">Welcome</h1>
